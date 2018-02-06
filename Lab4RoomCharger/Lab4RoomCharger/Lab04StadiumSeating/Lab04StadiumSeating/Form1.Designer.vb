a1a17f4e84c1120aa06c5a1c847ac74f05ccf0e6
Xavier McDaniel <S01051073@acad.tri-c.edu>
2018-02-06 14:19:20 -0500

    Finished Assignment

---
+++ Lab4RoomCharger/Lab4RoomCharger/Lab04StadiumSeating/Lab04StadiumSeating/Form1.Designer.vb
@@ -0,0 +1,273 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtClassA = New System.Windows.Forms.TextBox()
+        Me.txtClassB = New System.Windows.Forms.TextBox()
+        Me.txtClassC = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblClassA = New System.Windows.Forms.Label()
+        Me.lblClassB = New System.Windows.Forms.Label()
+        Me.lblClassC = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtClassC)
+        Me.GroupBox1.Controls.Add(Me.txtClassB)
+        Me.GroupBox1.Controls.Add(Me.txtClassA)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 158)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Tickets Sold"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lblTotal)
+        Me.GroupBox2.Controls.Add(Me.Label11)
+        Me.GroupBox2.Controls.Add(Me.lblClassC)
+        Me.GroupBox2.Controls.Add(Me.lblClassB)
+        Me.GroupBox2.Controls.Add(Me.lblClassA)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Location = New System.Drawing.Point(236, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(235, 158)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Revenue Generated"
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(35, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(145, 38)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter the number of tickets sold for each class of seats"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(38, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Class A:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(38, 95)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Class B:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(38, 123)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Class C:"
+        '
+        'txtClassA
+        '
+        Me.txtClassA.Location = New System.Drawing.Point(100, 66)
+        Me.txtClassA.Name = "txtClassA"
+        Me.txtClassA.Size = New System.Drawing.Size(50, 20)
+        Me.txtClassA.TabIndex = 4
+        '
+        'txtClassB
+        '
+        Me.txtClassB.Location = New System.Drawing.Point(100, 92)
+        Me.txtClassB.Name = "txtClassB"
+        Me.txtClassB.Size = New System.Drawing.Size(50, 20)
+        Me.txtClassB.TabIndex = 5
+        '
+        'txtClassC
+        '
+        Me.txtClassC.Location = New System.Drawing.Point(100, 120)
+        Me.txtClassC.Name = "txtClassC"
+        Me.txtClassC.Size = New System.Drawing.Size(50, 20)
+        Me.txtClassC.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(66, 37)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(45, 13)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Class A:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(66, 66)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(45, 13)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "Class B:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(66, 95)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(45, 13)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "Class C:"
+        '
+        'lblClassA
+        '
+        Me.lblClassA.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblClassA.Location = New System.Drawing.Point(117, 36)
+        Me.lblClassA.Name = "lblClassA"
+        Me.lblClassA.Size = New System.Drawing.Size(100, 23)
+        Me.lblClassA.TabIndex = 3
+        '
+        'lblClassB
+        '
+        Me.lblClassB.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblClassB.Location = New System.Drawing.Point(117, 65)
+        Me.lblClassB.Name = "lblClassB"
+        Me.lblClassB.Size = New System.Drawing.Size(100, 23)
+        Me.lblClassB.TabIndex = 4
+        '
+        'lblClassC
+        '
+        Me.lblClassC.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblClassC.Location = New System.Drawing.Point(117, 95)
+        Me.lblClassC.Name = "lblClassC"
+        Me.lblClassC.Size = New System.Drawing.Size(100, 23)
+        Me.lblClassC.TabIndex = 5
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(30, 127)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(81, 13)
+        Me.Label11.TabIndex = 6
+        Me.Label11.Text = "Total Revenue:"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotal.Location = New System.Drawing.Point(117, 126)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotal.TabIndex = 7
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(26, 197)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(125, 23)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "C&alculate Revenue"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(175, 197)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(125, 23)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clea&r"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(328, 197)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(125, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 235)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtClassC As TextBox
+    Friend WithEvents txtClassB As TextBox
+    Friend WithEvents txtClassA As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents lblClassC As Label
+    Friend WithEvents lblClassB As Label
+    Friend WithEvents lblClassA As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Lab4RoomCharger/Lab4RoomCharger/Lab04StadiumSeating/Lab04StadiumSeating/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim decClassATickets As Decimal
+        Dim decClassBTickets As Decimal
+        Dim decClassCTickets As Decimal
+        Dim decTotal As Decimal
+
+        Try
+            decClassATickets = CDec(txtClassA.Text) * 15
+            lblClassA.Text = decClassATickets.ToString("c")
+
+            decClassBTickets = CDec(txtClassB.Text) * 12
+            lblClassB.Text = decClassBTickets.ToString("c")
+
+            decClassCTickets = CDec(txtClassC.Text) * 9
+            lblClassC.Text = decClassCTickets.ToString("c")
+
+            decTotal = decClassATickets + decClassBTickets +
+                decClassCTickets
+            lblTotal.Text = decTotal.ToString("c")
+
+            lblTotal.BackColor = Color.Blue
+            lblTotal.ForeColor = Color.White
+
+        Catch
+            MessageBox.Show("All input must be valid numeric values.")
+        End Try
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtClassA.Clear()
+        txtClassB.Clear()
+        txtClassC.Clear()
+
+        lblClassA.Text = String.Empty
+        lblClassB.Text = String.Empty
+        lblClassC.Text = String.Empty
+        lblTotal.Text = String.Empty
+
+        lblTotal.BackColor = SystemColors.Control
+        lblTotal.ForeColor = SystemColors.ControlText
+
+        txtClassA.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
